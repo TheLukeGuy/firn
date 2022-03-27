45961be3c993cf8f9a07dc5becdeba3f57b1da73
Luke Chambers <consolelogluke@gmail.com>
2022-03-27 08:57:28 -0400

    :art: Move flags into its own module

--- src/arch/x86.rs
+++ src/arch/x86.rs
@@ -1,12 +1,14 @@
 pub mod cpu;
 pub mod device;
+pub mod flags;
 pub mod instr;
 pub mod modrm;
 pub mod opcodes;
 pub mod regs;
 
-pub use cpu::{Cpu, Flags};
+pub use cpu::Cpu;
 pub use device::{Device, IoInstr, PortMatchResult};
+pub use flags::Flags;
 pub use instr::{Instr, InstrFunc};
 pub use modrm::{Displacement, Modrm, ModrmRegType, RegMem, RmPtr};
 pub use regs::{GeneralByteReg, GeneralReg, GeneralWordReg, Reg, SegmentReg, WordReg};
--- src/arch/x86/cpu.rs
+++ src/arch/x86/cpu.rs
@@ -1,83 +1,10 @@
 use crate::arch::x86::SegmentReg::{Cs, Ds, Es, Ss};
 use crate::arch::x86::{
-    Device, GeneralByteReg, Instr, IoInstr, PortMatchResult, SegmentReg, WordReg,
+    Device, Flags, GeneralByteReg, Instr, IoInstr, PortMatchResult, SegmentReg, WordReg,
 };
 use crate::{cpu, System};
 use std::io;
 use std::io::Write;
-
-pub struct Flags {
-    pub carry: bool,
-    pub parity: bool,
-    pub adjust: bool,
-    pub zero: bool,
-    pub sign: bool,
-    pub trap: bool,
-    pub interrupt: bool,
-    pub direction: bool,
-    pub overflow: bool,
-}
-
-impl Flags {
-    pub fn new() -> Self {
-        Self {
-            carry: false,
-            parity: false,
-            adjust: false,
-            zero: false,
-            sign: false,
-            trap: false,
-            interrupt: false,
-            direction: false,
-            overflow: false,
-        }
-    }
-
-    pub fn set_parity_from_u8(&mut self, value: u8) {
-        self.parity = value.count_ones() % 2 == 0;
-    }
-
-    pub fn set_parity_from_u16(&mut self, value: u16) {
-        let lsb = (value & 0xff) as u8;
-        self.set_parity_from_u8(lsb);
-    }
-
-    // TODO: Methods for setting AF
-
-    pub fn set_zero_from_u8(&mut self, value: u8) {
-        self.zero = value == 0;
-    }
-
-    pub fn set_zero_from_u16(&mut self, value: u16) {
-        self.zero = value == 0;
-    }
-
-    pub fn set_sign_from_u8(&mut self, value: u8) {
-        self.sign = (value as i8).is_negative();
-    }
-
-    pub fn set_sign_from_u16(&mut self, value: u16) {
-        self.sign = (value as i16).is_negative();
-    }
-
-    pub fn set_pzs_from_u8(&mut self, value: u8) {
-        self.set_parity_from_u8(value);
-        self.set_zero_from_u8(value);
-        self.set_sign_from_u8(value);
-    }
-
-    pub fn set_pzs_from_u16(&mut self, value: u16) {
-        self.set_parity_from_u16(value);
-        self.set_zero_from_u16(value);
-        self.set_sign_from_u16(value);
-    }
-}
-
-impl Default for Flags {
-    fn default() -> Self {
-        Self::new()
-    }
-}
 
 pub struct Cpu {
     pub system: System,
